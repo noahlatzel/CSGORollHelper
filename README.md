6f6cb29e06571b2b3ffa89f556243f6acec56d59
noahlatzel <noahjlatzel@gmail.com>
2023-06-27 16:43:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# HSP Helper
+This Chrome Extension allows you to easily book sports courses at the University of Münster by just checking a checkbox. It saves you time by automating the process of filling out the form and submitting it.
+
+## Installation
+1. Download the source code from the Github repository.
+2. Open Google Chrome and go to [chrome://extensions](chrome://extensions).
+3. Turn on developer mode using the toggle switch in the top right corner.
+4. Click on the "Load unpacked" button in the top left corner.
+5. Select the folder where you downloaded the source code.
+6. The extension should now be installed and ready to use.
+## Usage
+1. Go to https://www.uni-muenster.de/Hochschulsport/ and navigate to the course booking page.
+2. Click on the checkbox next to the course you want to book.
+3. Fill out the personal information form that appears when you click on the extensions symbol (puzzle piece, left of downloads symbol) at the top right corner of Google Chrome.
+4. Click on the "Speichern" button. Your personal information is now stored locally on your pc and will only be send to the HSP servers. If you wish to remove your data, just clear the form and click "Speichern".
+5. The extension will automatically pull the data from the local storage and handle the rest.
+6. You should receive a confirmation message if the booking was successful.
+## Troubleshooting
+If you encounter any issues while using the extension, please try the following:
+
+* Make sure you have the latest version of Google Chrome installed.
+* Clear your browser cache and cookies.
+* Disable any other extensions that may be conflicting with this one.
+* Check the console log for error messages.
+## Contributing
+If you find a bug or would like to contribute to this project, please open an issue or submit a pull request on Github.
+
+## License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ content.js
@@ -0,0 +1,151 @@
+let doubleGreenFlag = false;
+let webhookURL = 'https://discord.com/api/webhooks/1123197528314224640/O1XhhgCiC_4PKOx6bk3hjmNbMTJUs0_4ujRE3BTi6SKW_NfuVdPXyGMUerTlK1z9hk4j';
+
+// Function to get an element by XPath
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+// Function to extract, parse, and print
+function extractJackpot() {
+    // Get the element
+    let path = '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/header/div/cw-roulette-jackpot/div/div/cw-pretty-balance/span/text()';
+    let jackpotElement = getElementByXpath(path);
+
+    if (jackpotElement !== null) {
+        // Get the text content, trim it, remove commas, parse it to float, and print it to the console
+        let jackpotText = jackpotElement.textContent.trim().replace(/,/g, '');
+        let jackpotFloat = parseFloat(jackpotText);
+    
+        if (!isNaN(jackpotFloat)) {
+					console.log('Current jackpot: ' + jackpotFloat);
+					return jackpotFloat;
+        } else {
+					console.log('Error: The text content of the jackpotElement could not be parsed to a float');
+        }
+    } else {
+        console.log('Error: No element found with the given XPath');
+    }
+		return 0.0;
+}
+
+function f(x, greenBet, jackpot) {
+	return (x / (greenBet + x) * jackpot / 3) - x;
+}
+
+function sendNotification(messageContent) {
+	// Create a new XMLHttpRequest
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", webhookURL, true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+
+	// Send a message
+	xhr.send(JSON.stringify({
+			content: messageContent
+	}));
+}
+
+function getOptimalBet(greenBet, jackpot) {
+	let maxVal = -Infinity;
+	let maxX = -1;
+	
+	for(let x = 0; x <= jackpot / 3 - greenBet; x += 0.1) {
+		// Calculate function
+		let val = f(x, greenBet, jackpot);
+		
+		if(val > maxVal) {
+			maxVal = val;
+			maxX = x;
+		}
+	}
+	return maxX;
+}
+
+function extractCountdown() {
+	// Get countdown by selector
+	let countdownPath = '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/div[2]/div[1]/cw-next-roll/div/div[2]';
+	let countdown = getElementByXpath(countdownPath);
+	if (countdown !== null) {
+		// Get the text content and print it to the console
+		let countdownText = countdown.textContent.trim();
+		let countdownFloat = parseFloat(countdownText);
+		if (!isNaN(countdownFloat)) {
+			if(countdownFloat >= 59.9) {
+				doubleGreenFlag = true;
+				sendNotification('INFO: Double Green detected!');
+				console.log('INFO: Double Green detected!');
+			}
+			if(countdownFloat <= 0.1 && countdownFloat > 0) {
+				let jackpot = extractJackpot();
+				let greenBet = extractGreenBet();
+				if(doubleGreenFlag) {
+					if(jackpot > 0 && greenBet > 0) {
+						let optimalBet = getOptimalBet(greenBet, jackpot);
+						betValue(optimalBet);
+						sendNotification('Current jackpot is ' + jackpot + '\nOptimal bet on green is ' + optimalBet.toFixed(2) + '\nProfit from jackpot is ' + f(optimalBet, greenBet, jackpot).toFixed(2));
+						console.log('Current jackpot is ' + jackpot + '\nOptimal bet on green is ' + optimalBet.toFixed(2) + '\nProfit from jackpot is ' + f(optimalBet, greenBet, jackpot).toFixed(2));
+					}
+					doubleGreenFlag = false;
+				}
+			}
+		} else {
+			console.log('Error: The countdown could not be parsed to a float');
+		}
+	} else {
+			console.log('Error: Countdown not found!');
+	}
+}
+
+// Function to extract, parse, and print the total green bet value
+function extractGreenBet() {
+    // Get the element
+    let path = '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/div[2]/div[2]/cw-roulette-bet-list/section[2]/header/div[2]/cw-pretty-balance/span/text()';
+    let greenBetElement = getElementByXpath(path);
+
+    if (greenBetElement !== null) {
+        // Get the text content, trim it, remove commas, parse it to float, and print it to the console
+        let greenBetText = greenBetElement.textContent.trim().replace(/,/g, '');
+        let greenBetFloat = parseFloat(greenBetText);
+    
+        if (!isNaN(greenBetFloat)) {
+            console.log('Current green bet: ' + greenBetFloat);
+						return greenBetFloat;
+        } else {
+            console.log('Error: The text content of the greenBetElement could not be parsed to a float');
+        }
+    } else {
+        console.log('Error: No element found with the given XPath');
+    }
+		return 0.0;
+}
+
+function betValue(bet) {
+	// XPath of the input element
+	let inputXPath = '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/div[2]/cw-amount-input/mat-form-field/div/div[1]/div[3]/div[2]/input';
+	
+	// Get the input element
+	let inputElement = getElementByXpath(inputXPath);
+
+	if (inputElement !== null) {
+		// Set the value of the input to x
+		inputElement.value = bet;
+		// XPath of the button element
+		let buttonXPath = '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/div[2]/div[2]/section/cw-roulette-bet-button[2]/div/button';
+
+		// Get the button element
+		let buttonElement = getElementByXpath(buttonXPath);
+
+		if (buttonElement !== null) {
+				// "Press" the button
+				//buttonElement.click();
+				console.log('INFO: Bet on green with ' + bet + ' coins');
+		} else {
+				console.log('Error: No element found with the given XPath for button');
+		}
+	} else {
+			console.log('Error: No element found with the given XPath');
+	}
+}
+
+// Call extractCountdown every 100 milliseconds (0.1 seconds)
+setInterval(extractCountdown, 100);
